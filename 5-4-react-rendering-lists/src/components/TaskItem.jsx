0f206a71e5cb3dd52735f1e977bd29a136f430f2
m3sh4al <aharbi419@gmail.com>
2026-02-25 09:57:49 +0300

    Task 2 Done

--- 5-4-react-rendering-lists/src/components/TaskItem.jsx
+++ 5-4-react-rendering-lists/src/components/TaskItem.jsx
@@ -5,6 +5,7 @@ export default function TaskItem({ task, onToggle, onDelete }) {
   return (
     <li className="task">
       <label className="taskMain">
+
         {/* TODO (TASK 4): Checkbox calls onToggle(task.id) on change
             Also make it reflect task.isDone using checked={task.isDone} */}
         <input type="checkbox" />
@@ -16,6 +17,8 @@ export default function TaskItem({ task, onToggle, onDelete }) {
         {/* TODO (TASK 2): Display the task title
             Example:
               <span className="title">{task.title}</span> */}
+
+<span className="title">{task.title}</span>
       </label>
 
       {/* TODO (TASK 4): Delete button calls onDelete(task.id) on click */}
